830be0fb0aaa0fb4c76a5e8863ad24418c465cf5
Kaiqui Benevenutti <benevenuttikaiqui@gmail.com>
2025-03-19 14:17:13 -0300

    removendo escalonamento

---
+++ src/main/java/Principal/Principal.java
@@ -0,0 +1,37 @@
+package Principal;
+
+import models.DadosSerie;
+import models.DadosTemporada;
+import services.ConsumoApi;
+import services.ConversorDados;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal{
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=76536016";
+    private ConsumoApi api = new ConsumoApi();
+    private ConversorDados conversor = new ConversorDados();
+    private List<DadosTemporada> temporadas = new ArrayList<>();
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void exibeMenu(){
+        System.out.println("Digite o nome da serie: ");
+        var nome = scanner.nextLine();
+        var json = api.obterDados(ENDERECO + nome.replace(" ", "+") + API_KEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+        for(var i = 1; i <= dados.totalTemporadas(); i++){
+            json = api.obterDados(ENDERECO + nome.replace(" ", "+") + "&Season=" + i + API_KEY);
+            DadosTemporada temporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(temporada);
+        }
+
+        temporadas.forEach(System.out::println);
+    }
+}
--- src/main/java/com/kaiqui/screenMatch/ScreenMatchApplication.java
+++ src/main/java/com/kaiqui/screenMatch/ScreenMatchApplication.java
@@ -1,5 +1,6 @@
 package com.kaiqui.screenMatch;
 
+import Principal.Principal;
 import models.DadosEpisodio;
 import models.DadosSerie;
 import models.DadosTemporada;
@@ -21,31 +22,7 @@ public class ScreenMatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		ConsumoApi api = new ConsumoApi();
-
-		var json = api.obterDados("https://www.omdbapi.com/?t=The+Boys&apikey=76536016");
-		System.out.println(json);
-
-		ConversorDados conversor = new ConversorDados();
-
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-
-		System.out.println(dados);
-
-		json = api.obterDados("https://www.omdbapi.com/?t=The+Boys&episode=6&season=2&apikey=76536016");
-
-		DadosEpisodio ep = conversor.obterDados(json, DadosEpisodio.class);
-
-		System.out.println(ep);
-
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-		for(var i = 0; i <= dados.totalTemporadas(); i++){
-			json = api.obterDados("https://www.omdbapi.com/?t=The+Boys&season=" + i + "&apikey=76536016");
-			DadosTemporada temporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(temporada);
-		}
-
-		temporadas.forEach(System.out::println);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
