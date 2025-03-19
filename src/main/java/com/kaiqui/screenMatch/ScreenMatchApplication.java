fbabe387e0980969911b522cbd12909fb4967b66
Kaiqui Benevenutti <benevenuttikaiqui@gmail.com>
2025-03-19 10:23:07 -0300

    configurando

--- src/main/java/com/kaiqui/screenMatch/ScreenMatchApplication.java
+++ src/main/java/com/kaiqui/screenMatch/ScreenMatchApplication.java
@@ -1,13 +1,18 @@
 package com.kaiqui.screenMatch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ScreenMatchApplication {
+public class ScreenMatchApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenMatchApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Projeto iniciado com sucesso");
+	}
 }
